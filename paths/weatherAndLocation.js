e6d82e239d1910f4bfdcb0e6574d1bf8dbbe46ea
meetantariksh <meetantariksh@gmail.com>
2017-07-02 19:48:42 +0530

    Update

--- paths/weatherAndLocation.js
+++ paths/weatherAndLocation.js
@@ -1,21 +1,18 @@
 var express = require('express');
 var router = express.Router();
 var http = require('request');
-var getIP = require('external-ip')();
+var getIP = publicIp = require('public-ip');
 
 router.get('/getLocation', function(req, res, next){
-    getIP((err, ip) => {
-    if (err) {
-        throw err;
-    }
-    console.log(ip);
+    publicIp.v4().then(ip => {
+        console.log(ip);
         http('http://ip-api.com/json/'+ip,  function (error, response, body) {
             if(error){
                 console.log(error);
             }else{
                 res.json(JSON.parse(response.body));
             }
-        });
+        }); 
     });
 });
 

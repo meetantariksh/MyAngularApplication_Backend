826e5ca8f5bf2173111f3c8071fab748b9fe86ba
meetantariksh <meetantariksh@gmail.com>
2017-07-08 14:11:37 +0530

    update

--- paths/news.js
+++ paths/news.js
@@ -2,8 +2,58 @@ var express = require('express');
 var router = express.Router();
 var http = require('request');
 
-router.get('/getNews', function(req, res, next){
-    http('https://newsapi.org/v1/articles?source=the-hindu&apiKey=d96de1b2807b4c118aca85ecdbe7d4da',  function (error, response, body) {
+router.get('/getNews/Global', function(req, res, next){
+    http('https://newsapi.org/v1/articles?source=google-news&sortBy=top&apiKey=d96de1b2807b4c118aca85ecdbe7d4da',  function (error, response, body) {
+        if(error){
+            console.log(error);
+        }else{
+            res.json(JSON.parse(response.body));
+        }
+    });
+});
+
+router.get('/getNews/India', function(req, res, next){
+    http('https://newsapi.org/v1/articles?source=the-times-of-india&sortBy=top&apiKey=d96de1b2807b4c118aca85ecdbe7d4da',  function (error, response, body) {
+        if(error){
+            console.log(error);
+        }else{
+            res.json(JSON.parse(response.body));
+        }
+    });
+});
+
+router.get('/getNews/Business', function(req, res, next){
+    http('https://newsapi.org/v1/articles?source=the-economist&sortBy=top&apiKey=d96de1b2807b4c118aca85ecdbe7d4da',  function (error, response, body) {
+        if(error){
+            console.log(error);
+        }else{
+            res.json(JSON.parse(response.body));
+        }
+    });
+});
+
+router.get('/getNews/Tech', function(req, res, next){
+    http('https://newsapi.org/v1/articles?source=hacker-news&sortBy=top&apiKey=d96de1b2807b4c118aca85ecdbe7d4da',  function (error, response, body) {
+        if(error){
+            console.log(error);
+        }else{
+            res.json(JSON.parse(response.body));
+        }
+    });
+});
+
+router.get('/getNews/Sports', function(req, res, next){
+    http('https://newsapi.org/v1/articles?source=espn&sortBy=top&apiKey=d96de1b2807b4c118aca85ecdbe7d4da',  function (error, response, body) {
+        if(error){
+            console.log(error);
+        }else{
+            res.json(JSON.parse(response.body));
+        }
+    });
+});
+
+router.get('/getNews/Entertainment', function(req, res, next){
+    http('https://newsapi.org/v1/articles?source=buzzfeed&sortBy=top&apiKey=d96de1b2807b4c118aca85ecdbe7d4da',  function (error, response, body) {
         if(error){
             console.log(error);
         }else{
